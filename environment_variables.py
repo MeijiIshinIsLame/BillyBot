3ed2dcea0466a3ed9042cd71fc7bda7c369d5393
meijiishinislame <meijiishinislame@gmail.com>
2020-03-29 02:02:37 -1000

    fucking env vars

--- environment_variables.py
+++ environment_variables.py
@@ -80,6 +80,6 @@ QmG4kKXDekE3dwHnqw8RIx9FvjnXggZboCHFDVRt1BJNozA=
 os.environ["DB_NAME"] = "billybot-test-images"
 os.environ["DB_USERNAME"] = "thequadehunter"
 os.environ["DB_PASSWORD"] = "Lllke477&"
-os.environ["DB_HOSTNAME"] = "35.185.240.59"
+os.environ["DB_HOSTNAME"] = "172.24.240.3"
 os.environ["DB_PORT"] = "5432"
 os.environ["SSL_MODE"] = "verify-ca"
