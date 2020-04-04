016cddadc1851476d81c462d863680881f962e14
meijiishinislame <meijiishinislame@gmail.com>
2020-04-03 23:36:41 -1000

    incest lel, master server

--- reddit_module.py
+++ reddit_module.py
@@ -11,7 +11,7 @@ async def get_incest_story():
 	max_length = 2000
 	min_length = 10
 	i = 0
-	while i < 200:
+	while i < 1500:
 		full_submission = "**{}**\n\n{}".format(submission.title, submission.selftext.strip("\t"))
 		if len(full_submission) < max_length and len(full_submission) > min_length:
 			return full_submission
