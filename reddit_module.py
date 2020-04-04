1fe4636e4b43bacb73abe24eec862a41d9926d66
meijiishinislame <meijiishinislame@gmail.com>
2020-04-04 02:39:06 -1000

    incest lel, master server, fixed images

--- reddit_module.py
+++ reddit_module.py
@@ -11,9 +11,10 @@ async def get_incest_story():
 	max_length = 2000
 	min_length = 10
 	i = 0
-	while i < 1500:
+	while i < 10000:
 		full_submission = "**{}**\n\n{}".format(submission.title, submission.selftext.strip("\t"))
 		if len(full_submission) < max_length and len(full_submission) > min_length:
-			return full_submission
+			if "https://imgur.com/" not in full_submission:
+				return full_submission
 		i += 1
 	return "Could not fetch submission."
