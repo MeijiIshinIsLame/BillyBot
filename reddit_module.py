38bdd3005d0bc3297f5d25db66c2afff9f64ac72
meijiishinislame <meijiishinislame@gmail.com>
2020-04-04 04:04:14 -1000

    incest lel, master server, fixed images

--- reddit_module.py
+++ reddit_module.py
@@ -2,19 +2,23 @@ import praw
 import os
 import time
 import asyncio
+import random
 
 reddit = praw.Reddit(client_id=os.environ["reddit_id"],
 	client_secret=os.environ["reddit_secret"], user_agent=os.environ["reddit_agent"])
 
 async def get_incest_story():
-	submission = reddit.subreddit("gayincest").random()
-	max_length = 2000
+	max_length = 1900
 	min_length = 10
-	i = 0
-	while i < 10000:
-		full_submission = "**{}**\n\n{}".format(submission.title, submission.selftext.strip("\t"))
-		if len(full_submission) < max_length and len(full_submission) > min_length:
+	valid_submissions = []
+
+	for submission in reddit.subreddit("gayincest").hot(limit=100000):
+		if len(submission.selftext) < max_length and len(submission.selftext) > min_length:
 			if "https://imgur.com/" not in full_submission:
-				return full_submission
-		i += 1
-	return "Could not fetch submission."
+				valid_submissions.append(submission)
+
+	if valid_submissions:
+		full_submission = "**{}**\n\n{}".format(submission.title, submission.selftext.strip("\t"))
+		return full_submission
+	else:
+		return "Could not fetch submission."
